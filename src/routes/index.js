ee467a418a367744946bd75022dbe9d389cc07e3
Junior Juarez Centeno <juniorj@microsoft.com>
2022-01-25 15:55:01 -0600

    Added a DB connection pool:Max Pool Size=20;Min Pool Size=0;Connection Lifetime=30000

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,17 +3,26 @@ const sql = require("mssql");
 const router = Router();
 
 // Use a custom connection string from Azure App Service -> App Settings -> CUSTOMCONNSTR_<ConnectionStringName>
-var sqlConnString = process.env.CUSTOMCONNSTR_customscript1;
+const sqlConnString = process.env.CUSTOMCONNSTR_customscript1;
+
+// Create SQL connection pool: Pooling=true;Max Pool Size=20;Min Pool Size=0;Connection Lifetime=30000
+const pool1 = new sql.ConnectionPool(sqlConnString);
+const pool1Connect = pool1.connect();
 
 async function getPersonas () {
+    
     try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(sqlConnString)
-        const result = await sql.query`SELECT * FROM [dbo].Person`
+        await pool1Connect; // ensures that the pool has been created
+
+        const request = pool1.request(); // or: new sql.Request(pool1)
+        const result = await request.query('SELECT * FROM [dbo].Person');
         router.get('/', (req, res) => res.json({message: "Hello " + result.recordset[0].MiddleInitial + " " + result.recordset[0].FirstName}));
+        
     } catch (err) {
         console.log("Error: ", err)
     }
+    
+    pool1.close();
 }
 
 getPersonas();
